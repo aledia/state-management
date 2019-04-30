2af34326dea5f7d546ae5d5018fac2db5fdd5c22
Daniel Vicario <danigv.email@gmail.com>
2019-04-30 10:26:36 +0200

    first version, containing clickCounter increase

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    clickTimes : 0
+  }
+
+  // constructor () {
+  //   super();
+
+  //   this.state = {
+  //     clickTimes : 0
+  //   }
+  // }
+
+  increaseCounter(param) {
+    this.setState({
+      clickTimes : this.state.clickTimes + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.clickTimes}
+        <h1>Hola ironhackers! {this.state.clickTimes}</h1>{this.state.clickTimes}
+        {/* <button onClick={this.increaseCounter.bind(this)}>click me to increase counter 1!</button> */}
+        <button onClick={() => this.increaseCounter(999999)}>click me to increase counter!</button>
+        {this.state.clickTimes}
+        xxxx
+        {this.state.clickTimes}
+        </div>
+    )
+  }
 }
 
 export default App;
